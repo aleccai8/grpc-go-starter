c8043b13bf14f32f9fa4af356f5499cecb2b2fd7
zhengheng7913 <1124381365@qq.com>
2022-02-05 04:40:08 -0500

    feat: pluginType

---
+++ client/client.go
@@ -0,0 +1,56 @@
+package client
+
+import "context"
+
+var (
+	constructors = make(map[string]func() Client)
+)
+
+func init() {
+	constructors[GrpcProtocol] = NewGrpcClient
+	constructors[HttpProtocol] = NewHttpClient
+}
+
+func WithDiscovery(name string) Option {
+	return func(opt *Options) {
+		opt.Discovery = name
+	}
+}
+
+func WithName(name string) Option {
+	return func(opt *Options) {
+		opt.Name = name
+	}
+}
+
+func WithNamespace(namespace string) Option {
+	return func(opt *Options) {
+		opt.Namespace = namespace
+	}
+}
+
+type Options struct {
+	Discovery string
+	Name      string
+	Namespace string
+}
+
+type Option func(opt *Options)
+
+type Client interface {
+	Invoke(context context.Context, method string, req interface{}, opts ...Option) (interface{}, error)
+
+	Register(realClient interface{}, opts ...Option)
+}
+
+type Manager struct {
+	clients map[string]Client
+}
+
+func (m *Manager) AddClient(name string, client Client) {
+	m.clients[name] = client
+}
+
+func (m *Manager) Client(name string) Client {
+	return m.clients[name]
+}
